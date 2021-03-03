20f1f8d57129a19f288df267a2c62095f4f9dd3b
lunker <zhongdingshou120@163.com>
2021-03-03 17:37:20 +0800

    Update README.md

--- api.js
+++ api.js
@@ -1,13 +1,13 @@
 import easyAxiosInstance from 'easyAxiosInstance.js'
 
 // 管理员相关接口
-const login        = (params)     => easyAxiosInstance.POST('/login', params)
-const adminList    = (params)     => easyAxiosInstance.GET('/admin', params)
-const addAdmin     = (params)     => easyAxiosInstance.POST('/admin', params)
-const updateAdmin  = (id, params) => easyAxiosInstance.PUT(`/admin/${id}`, params)
+const login        = (param)     => easyAxiosInstance.POST('/login', param)
+const adminList    = (param)     => easyAxiosInstance.GET('/admin', param)
+const addAdmin     = (param)     => easyAxiosInstance.POST('/admin', param)
+const updateAdmin  = (id, param) => easyAxiosInstance.PUT(`/admin/${id}`, param)
 const deleteAdmin  = (id)         => easyAxiosInstance.DELETE(`/admin/${id}`)
 const deletesAdmin = (list)       => easyAxiosInstance.DELETE(`/admin/`, list)
-const uploadFile   = (params)     => easyAxiosInstance.FORMDATA(`/admin/`, params)
+const uploadFile   = (param)     => easyAxiosInstance.FORMDATA(`/admin/`, param)
 
 export { 
     login,
