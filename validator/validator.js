34ca32ecca65eb32f12261d393b38756c931480f
zhongdingshou <xiu_formal@163.com>
2021-03-04 15:27:34 +0800

    修改验证器的错误提示内容

--- validator/validator.js
+++ validator/validator.js
@@ -3,7 +3,7 @@ import { toRawType } from '../utils/utils.js'
 export const validator =  (options, claim) => {
     if(toRawType(options) !== 'object') return {
         illegal: true,
-        errorMsg: '参数 options 数据类型不是 object，请检查'
+        errorMsg: '参数 options 的数据类型不是 object，请检查'
     }
     
     return checkSome(options, claim)
@@ -14,13 +14,13 @@ const checkSome = (options, claim) => {
         // 验证类型
         if (options.hasOwnProperty(item) && (toRawType(options[item]) !== claim[item].type)) return {
             illegal: true,
-            errorMsg: `参数 ${item} 数据类型不是 ${claim[item].type}，请检查`
+            errorMsg: `全局配置 ${item} 的数据类型不是 ${claim[item].type}，请检查`
         }
 
         // 验证范围
         if (claim[item].hasOwnProperty('enum') && !claim[item].enum.includes(options[item])) return {
             illegal: true,
-            errorMsg: `参数 ${item} 的值不是 ${claim[item].enum.join('，')}，请检查`
+            errorMsg: `全局配置 ${item} 的值不是 ${claim[item].enum.join('，')}，请检查`
         }
 
         // 验证属性值
