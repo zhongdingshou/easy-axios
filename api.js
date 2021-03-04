f9df3abd28fa88eddc3fcf595e38cd630e003dce
zhongdingshou <79820937+zhongdingshou@users.noreply.github.com>
2021-03-04 13:35:42 +0800

    Update api.js

--- api.js
+++ api.js
@@ -5,8 +5,8 @@ const login        = (param)     => easyAxiosInstance.POST('/login', param)
 const adminList    = (param)     => easyAxiosInstance.GET('/admin', param)
 const addAdmin     = (param)     => easyAxiosInstance.POST('/admin', param)
 const updateAdmin  = (id, param) => easyAxiosInstance.PUT(`/admin/${id}`, param)
-const deleteAdmin  = (id)         => easyAxiosInstance.DELETE(`/admin/${id}`)
-const deletesAdmin = (list)       => easyAxiosInstance.DELETE(`/admin/`, list)
+const deleteAdmin  = (id)        => easyAxiosInstance.DELETE(`/admin/${id}`)
+const deletesAdmin = (list)      => easyAxiosInstance.DELETE(`/admin/`, list)
 const uploadFile   = (param)     => easyAxiosInstance.FORMDATA(`/admin/`, param)
 
 export { 
