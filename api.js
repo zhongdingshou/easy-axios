c2cf08873ba3fee4205bcd6e9fc9c1ddd2d433f4
zhongdingshou <xiu_formal@163.com>
2021-03-04 14:17:44 +0800

    update

--- api.js
+++ api.js
@@ -1,20 +1,14 @@
 import easyAxiosInstance from 'easyAxiosInstance.js'
 
-// 管理员相关接口
+// 示例，管理员相关接口
 const login        = (param)     => easyAxiosInstance.POST('/login', param)
 const adminList    = (param)     => easyAxiosInstance.GET('/admin', param)
-const addAdmin     = (param)     => easyAxiosInstance.POST('/admin', param)
-const updateAdmin  = (id, param) => easyAxiosInstance.PUT(`/admin/${id}`, param)
 const deleteAdmin  = (id)        => easyAxiosInstance.DELETE(`/admin/${id}`)
-const deletesAdmin = (list)      => easyAxiosInstance.DELETE(`/admin/`, list)
 const uploadFile   = (param)     => easyAxiosInstance.FORMDATA(`/admin/`, param)
 
 export { 
     login,
     adminList,
-    addAdmin,
-    updateAdmin,
     deleteAdmin,
-    deletesAdmin,
     uploadFile
 }
